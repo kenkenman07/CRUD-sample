2797477841cdd8a0baefe002670c39fdd6142e8e
kenkenman07 <ken0522g@icloud.com>
2026-07-02 09:31:50 +0900

    feat: backend/index.jsを記述

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { router } from "./routes/index.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", router);
+
+app.listen(3000, function () {
+  console.log("server is running on port 3000");
+});
